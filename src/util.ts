4ad04606ba483a5a64d06500fe1a7a10599ceada
D-Sketon <2055272094@qq.com>
2025-02-12 21:25:13 +0800

    refactor(export): use createWriteStream to export json (#277)
    * perf(export): Speed up and reduce memory usage
    
    * refactor: use stream
    
    * Update src/database.ts
    
    Co-authored-by: Sukka <github@skk.moe>
    Signed-off-by: D-Sketon <2055272094@qq.com>
    
    * fix: test
    
    * fix: coverage
    
    ---------
    
    Signed-off-by: D-Sketon <2055272094@qq.com>
    Co-authored-by: Sukka <github@skk.moe>

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import { once } from 'node:events';
+import type { Writable } from 'node:stream';
+
 function extractPropKey(key: string): string[] {
   return key.split('.');
 }
@@ -190,4 +193,25 @@ export function parseArgs<B extends string | Record<string, number | Record<stri
   }
 
   return result;
+}
+
+/**
+ * A small utility function for writing chunk to a stream, and only return promise when needed (stream backpressure)
+ *
+ * ```ts
+ * const writeStream = fs.createWriteStream(outputFile);
+ * for (const line of source) {
+ *   const p = asyncWriteToStream(writeStream, line + '\n');
+ *   if (p) {
+ *     // eslint-disable-next-line no-await-in-loop -- stream backpressure
+ *     await p;
+ *   }
+ * }
+ */
+export function asyncWriteToStream<T>(stream: Writable, chunk: T): Promise<unknown> | null {
+  const waitDrain = !stream.write(chunk);
+  if (waitDrain) {
+    return once(stream, 'drain');
+  }
+  return null;
 }
